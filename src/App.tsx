7bfb45a750d30ee14931207bf144ea2d09366b57
Apratim Mahata <apra487@gmail.com>
2022-05-28 12:24:52 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,12 @@ import {
 
 import './assets/index.css';
 
-/* pages */
-// import Content from './container/content/Content'
 
 /* components */
 import Aliases from './components/aliases/Aliases';
 import Navbar from './components/navbar/Navbar';
 
-/* api */
-// import { getCustomers } from './api/chinook';
+/* pages */
 import CreatePage from './pages/createPage/CreatePage';
 
 
